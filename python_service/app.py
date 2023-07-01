04d2c21ebace2fad0f3adf62cfb0169455b7f419
benntuecon <zxc123598tw@gmail.com>
2023-06-30 20:32:24 -0700

    update hdfs

--- python_service/app.py
+++ python_service/app.py
@@ -15,10 +15,16 @@ def create_producer():
 producer = create_producer()
 with open(LOG_FILE, 'r') as file:
     print(f'starting reading file')
+    i = 0
     while log:= file.readline():
         log = log.encode('utf-8')
         producer.send('log', log)
-        print(f'sending log == > {log}')
+        i %= 100
+        if not i :
+            print('sending ===> {log}')
         time.sleep(0.1)  # Pause to avoid overloading Kafka
+        i +=1
+
+
 
 producer.close()
